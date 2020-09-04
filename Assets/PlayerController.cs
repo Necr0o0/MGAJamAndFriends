8564b93c8a1177e1f40c10cc240756649f64d2f3
Necr0o0 <radekklaser@gmail.com>
2020-09-04 23:56:04 +0200

    Weapon system

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -7,45 +8,43 @@ public class PlayerController : MonoBehaviour
 {
     private float minX = 60f;
     private float maxX = 60f;
-    Vector2 moveCamera = new Vector2();
+    private Vector2 moveCamera;
+    private Rigidbody rb;
+    private Gamepad gamepad;
+    private Transform camera;
+    private Transform player;
+    public WeaponController weapon;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        camera = transform.GetChild(0);
+        player = transform;
+    }
 
     void Update()
     {
-        var gamepad = Gamepad.current;
+        gamepad = Gamepad.current;
         if (gamepad == null)
         {
-            Debug.Log("No Controller");
             return; // No gamepad connected.
         }
-
-        var mouse = Mouse.current;
-        
-        if (mouse == null)
-        {
-            Debug.Log("No Mouse!");
-            return; // No gamepad connected.
-        }   
 
         if (gamepad.rightTrigger.wasPressedThisFrame)
         {
-            Debug.Log("Shoot!");
+           weapon.Shoot();
         }
-
-     
-        
         
         moveCamera += gamepad.rightStick.ReadValue();
         moveCamera.y = Mathf.Clamp(moveCamera.y, -60f, 60f);
         
         
-        transform.GetChild(0).transform.localEulerAngles = new Vector3(-moveCamera.y,0,0);
-        transform.localEulerAngles = new Vector3(0,moveCamera.x,0);
+        camera.localEulerAngles = new Vector3(-moveCamera.y,0,0);
+        player.localEulerAngles = new Vector3(0,moveCamera.x,0);
 
         Vector2 move = gamepad.leftStick.ReadValue();
-        Debug.Log(move);
-        // 'Move' code here
-        var moveDir = (move.x * transform.right + move.y * transform.forward).normalized;
-        transform.GetComponent<Rigidbody>().velocity = moveDir * 10f ;
+        var moveDir = (move.x * player.right + move.y * player.forward).normalized;
+        rb.velocity = moveDir * 10f ;
       
 
 
---
+++ Assets/WeaponController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+    public int ammo;
+    public GameObject Bullet;
+
+
+    public void Shoot()
+    {
+        if (ammo > 0)
+        {
+            var bullet =  Instantiate(Bullet,transform.GetChild(0).position,Quaternion.identity);
+
+            bullet.transform.GetComponent<Rigidbody>().AddForce(transform.forward * 1000f);
+
+        }
+    }
+
+
+}
