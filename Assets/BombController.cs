ad27fa88b0726a91498c198e8a6489e0182a8f95
Necr0o0 <radekklaser@gmail.com>
2020-09-05 00:47:05 +0200

    DOTWeen + bombs

---
+++ Assets/BombController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class BombController : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Zara wybuchnie");
+        var sequence = DOTween.Sequence();
+        sequence.AppendInterval(2f);
+        sequence.OnComplete(() =>
+        {
+            Debug.Log("Wybuch!");
+            transform.gameObject.SetActive(false);
+        });
+        sequence.Play();
+    }
+
+
+}
--- Assets/WeaponController.cs
+++ Assets/WeaponController.cs
@@ -12,9 +12,9 @@ public class WeaponController : MonoBehaviour
     {
         if (ammo > 0)
         {
+            ammo--;
             var bullet =  Instantiate(Bullet,transform.GetChild(0).position,Quaternion.identity);
-
-            bullet.transform.GetComponent<Rigidbody>().AddForce(transform.forward * 1000f);
+            bullet.transform.GetComponent<Rigidbody>().AddForce(transform.GetChild(0).forward * 1000f);
 
         }
     }
