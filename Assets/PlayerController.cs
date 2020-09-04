405863a2fb8b83b117c25c6c3f036d3a86a68697
Necr0o0 <radekklaser@gmail.com>
2020-09-04 21:18:15 +0200

    Added InputController

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    void FixedUpdate()
+    {
+        var gamepad = Gamepad.current;
+        if (gamepad == null)
+        {
+            Debug.Log("No Controller");
+            return; // No gamepad connected.
+        }
+
+        var mouse = Mouse.current;
+        
+        if (mouse == null)
+        {
+            Debug.Log("No Mouse!");
+            return; // No gamepad connected.
+        }   
+
+        if (gamepad.rightTrigger.wasPressedThisFrame)
+        {
+            Debug.Log("Shoot!");
+        }
+
+        Vector2 move = gamepad.leftStick.ReadValue();
+        // 'Move' code here
+        transform.GetComponent<Rigidbody>().velocity = move;
+
+    }
+}
