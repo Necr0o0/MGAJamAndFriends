d3cc66ef75c96f20cb23e745068f512bd3388ca0
Necr0o0 <radekklaser@gmail.com>
2020-09-06 01:45:15 +0200

    ENDGAME

---
+++ Assets/ArenaBorderManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class ArenaBorderManager : MonoBehaviour
+{
+    private void OnTriggerExit(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
+}
