3e1229be14ed7e7005b5224e26f683a4fdf30310
Qarian <qarian25@gmail.com>
2020-09-05 02:21:28 +0200

    Added Enemies

---
+++ Assets/Scripts/Enemy/PlayerFinder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class PlayerFinder
+{
+    [SerializeField] private float sightRadius = 5f;
+    [SerializeField] private float sightAngle = 90f;
+
+    [SerializeField] private Transform tmpPlayerRef = default;
+    
+    public Transform Player { get; private set; }
+
+    public bool TryToFindPlayer(Transform origin)
+    {
+        // some kind of Player.transform
+        if (!tmpPlayerRef)
+            return false;
+        Transform playerTransform = tmpPlayerRef;
+        Vector3 distance = playerTransform.position - origin.position;
+        if (distance.magnitude > sightRadius)
+            return false;
+        float angle = Vector3.Angle(origin.forward, distance);
+        if (Mathf.Abs(angle) > sightAngle)
+            return false;
+
+        Player = playerTransform;
+        return true;
+    }
+}
---
+++ Assets/Scripts/Enemy/Types/HumanoidEnemy.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class HumanoidEnemy : Enemy
+{
+    [Space]
+    [SerializeField] private float waitTime = 2f;
+    [SerializeField] private float walkingTime = 3f;
+    
+    [Space]
+    [SerializeField] PlayerFinder playerFinder;
+    
+    private WaitForSeconds yieldWait;
+    private WaitForSeconds yieldWalk;
+
+    protected override void Awake()
+    {
+        playerFinder = new PlayerFinder();
+
+        yieldWait = new WaitForSeconds(waitTime);
+        yieldWalk = new WaitForSeconds(walkingTime);
+        
+        base.Awake();
+    }
+
+    protected override IEnumerator Movement()
+    {
+        while (playerFinder.Player is null)
+        {
+            movementToDo = null;
+            yield return yieldWait;
+            if (playerFinder.TryToFindPlayer(transform))
+                break;
+            transform.Rotate(Vector3.up, Random.Range(-200, 200), Space.Self);
+            movementToDo = MoveWithoutTarget;
+            yield return yieldWalk;
+            if (playerFinder.TryToFindPlayer(transform))
+                break;
+        }
+
+        movementToDo = MoveToTarget;
+    }
+
+    protected override void MoveWithoutTarget()
+    {
+        rb.MovePosition(transform.position + transform.forward * (Time.fixedDeltaTime * speed));
+    }
+
+    protected override void MoveToTarget()
+    {
+        Vector3 distance = transform.position - playerFinder.Player.position;
+        Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(distance.normalized), Time.deltaTime * 10f);
+    }
+}
